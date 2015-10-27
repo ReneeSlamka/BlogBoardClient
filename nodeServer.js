f6c4f03923e0ed74efc7aba289ec3fbdc5d85959
Renée <renee.slamka@gmail.com>
2015-10-27 15:50:10 -0700

    Working basic template setup with Swig for js

--- nodeServer.js
+++ nodeServer.js
@@ -1,12 +1,22 @@
 //Lets require/import the HTTP module
 var express = require('express');
 var app = express();
+var swig = require('swig');
 
 app.use('/static', express.static(__dirname + '/public'));
 app.use('/static', express.static(__dirname + '/pages'));
 
+// This is where all the magic happens!
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/pages');
+
+
+
 app.get("/", function(req, res) {
-	res.sendFile(__dirname + '/pages/index.html');
+	//res.sendFile(__dirname + '/pages/index.html');
+	res.render('index', {});
 });
 
 app.get("/home", function(req, res) {
