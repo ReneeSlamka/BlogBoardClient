3f0e1abdb78caa0fa4c9b5b3dd8d0dcfc87ed72f
Renée <renee.slamka@gmail.com>
2015-10-27 15:16:28 -0700

    Set up basic working nodeJS server

--- nodeServer.js
+++ nodeServer.js
@@ -1,48 +1,24 @@
 //Lets require/import the HTTP module
-var http = require('http');
-var dispatcher = require('httpdispatcher');
-var fs = require('fs');
+var express = require('express');
+var app = express();
 
-//Create a server
-var server = http.createServer(handleRequest);
+app.use('/static', express.static(__dirname + '/public'));
+app.use('/static', express.static(__dirname + '/pages'));
 
-//Lets define a port we want to listen to
-const PORT=3000; 
+app.get("/", function(req, res) {
+	res.sendFile(__dirname + '/pages/index.html');
+});
 
-//Lets start our server
-server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
+app.get("/home", function(req, res) {
+	res.sendFile(__dirname + '/pages/home.html');
 });
 
 
+var port = 3000;
 
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});
 
-//For all your static (js/css/images/etc.) set the directory name (relative path).
-dispatcher.setStatic('resources'); //*CHANGE LATER
-
-
-//We need a function which handles requests and send response
-//Used by createServer method
-function handleRequest(request, response){
-
-	if (request.method == 'GET' && request.url == '/') {
-		response.writeHead(200, {'Content-Type': 'text/html'});
-		fs.createReadStream("./pages/index.html").pipe(response);
-	}
-	else if (request.method == 'GET' && request.url == '/home') {
-		response.writeHead(200, {'Content-Type': 'text/html'});
-		fs.createReadStream("./pages/home.html").pipe(response);
-	}
-	else {
-		send404Response(response);
-	}
-}
-
-function send404Response(response) {
-	response.writeHead(404, {"Content-Type": "text/plain"});
-	response.write("Error 404: Page request could not be found. Baka.");
-	response.end();
-}
 
 
