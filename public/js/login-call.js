039ffa61655e6617f22d047711d1921e1c8d1ebf
Renée <renee.slamka@gmail.com>
2015-11-02 17:59:05 -0800

    Minor naming changes to login/create account functions

--- public/js/login-call.js
+++ public/js/login-call.js
@@ -1,31 +1,3 @@
-/**
- * Created by renee on 2015-10-27.
- */
-function getData(event) {
-    var $name = $("#new-username").val();
-    var $password = $("#new-password").val();
-
-    $.ajax({
-        cache: false,
-        async: false,
-        url: "http://localhost:8080/login",
-        data: {"username": $name, "password": $password},
-        dataType: 'json',
-        success: function(result){
-            alert("token recieved: " + result);
-            $('.account-name').append('<p> Name: ' + result.name + '</p>');
-        },
-        error: function(request, textStatus, errorThrown) {
-            alert(textStatus);
-        },
-        complete: function(request, textStatus) { //for additional info
-            alert(request.responseText);
-            alert(textStatus);
-            window.location.href = request.responseJSON.httpResponseHeader;
-
-        }
-    });
-}
 
 function createAccount() {
     var $newUsername = $("#new-username").val();
@@ -48,6 +20,31 @@ function createAccount() {
             alert(request.responseText);
             alert(textStatus);
             alert(request.responseJSON.httpResponseHeader);
+            window.location.href = request.responseJSON.httpResponseHeader;
+
+        }
+    });
+}
+
+function login(event) {
+    var $name = $("#login-username").val();
+    var $password = $("#login-password").val();
+
+    $.ajax({
+        cache: false,
+        async: false,
+        url: "http://localhost:8080/login",
+        data: {"username": $name, "password": $password},
+        dataType: 'json',
+        success: function(result){
+            alert("token recieved: " + result);
+        },
+        error: function(request, textStatus, errorThrown) {
+            alert(textStatus);
+        },
+        complete: function(request, textStatus) { //for additional info
+            alert(request.responseText);
+            alert(textStatus);
             window.location.href = request.responseJSON.httpResponseHeader;
 
         }
